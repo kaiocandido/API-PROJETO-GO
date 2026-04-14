e093b5fa9122bacc8f6c4cbcb0952fbe084c7a93
adm <kaio.candf@gmail.com>
2026-04-14 17:14:24 -0300

    fazendo configuração do env e variaveis de ambiente

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	router "api/src/Router"
+	"api/src/config"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
 	fmt.Println("Rodando API")
 
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
 }
---
+++ src/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// StringConexãoBanco usado para conexão com mysql
+	StringConexaoBanco = ""
+
+	// Porta indica a porta onde a aplicação vai rodar
+	Porta = 0
+)
+
+// Carregar inicializa as variaveis de ambiente
+func Carregar() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Porta, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NOME"),
+	)
+
+}
