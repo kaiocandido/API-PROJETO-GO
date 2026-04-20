ccff8c5e688c92d8c96992a4b6ced5e3620672d6
adm <kaio.candf@gmail.com>
2026-04-20 19:02:15 -0300

    ajuste em rota buscar

--- src/repository/publicações.go
+++ src/repository/publicações.go
@@ -79,7 +79,7 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]model.Publicacao, err
 		from publicacoes p
 		inner join usuarios u on u.id = p.autor_id
 		inner join seguidores s on p.autor_id = s.usuario_id
-		where u.id = ? or s.seguidor_id = ?
+		where u.id = ? or s.seguidor_id = ? order by 1 desc
 	`, usuarioID, usuarioID)
 
 	if err != nil {
